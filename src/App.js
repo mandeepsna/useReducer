bcf8731e353b4b835d9d1adae3ace468062f3996
mandeephanda <mandeephanda@gmail.com>
2023-06-28 18:35:44 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Toggle from './components/Toggle';
 
 function App() {
   return (
      <div>
-      <h2 className='text-primary'>Mandeep Handa</h2>
+    <Toggle></Toggle>
      </div>
   );
 }
