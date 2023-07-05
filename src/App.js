7f3cf6338d7e6a2d284b486c1d594ed85b00eb1d
mandeephanda <mandeep@stellartms.com>
2023-07-05 09:57:20 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useReducer } from "react";
+import "./App.css";
 
-import './App.css';
-import Toggle from './components/Toggle';
+const reduce = (initialState, action) => {
+       if(action.type==='increment') { return ++initialState }
+       else { return --initialState}; 
+  };
 
 function App() {
+
+  let [initialState, dispatch] = useReducer(reduce, 0);
   return (
-     <div>
-    <Toggle></Toggle>
-     </div>
+    <div className="col-md-5 mx-5 mt-5">
+      <button
+        className="btn btn-primary me-4"
+        onClick={() => {
+          dispatch({type:'increment'});
+        }}
+      >
+        Increment
+      </button>
+      <span>{initialState}</span>
+      <button
+        className="btn btn-danger mx-4"
+        onClick={() => {
+          dispatch({type:"decrement"});
+        }}
+      >
+        Decrement
+      </button>
+    </div>
   );
 }
 
